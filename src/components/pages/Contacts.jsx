e84152486877abb1c366e901874aef0b7df64329
Ilya Soloveychik <dj.active.17@gmail.com>
2024-06-17 16:13:05 +0300

    chore: about + contacts sample text

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,7 +1,13 @@
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div>
+        <h1>Contacts</h1>
+        <p><a href="https://wa.me/972535220057?text=%2Fstart" target="_blank">Whatsapp bot</a></p>
+        <p><a href="https://t.me/avielhelper_bot" target="_blank">Telegram bot</a></p>
+        <p><a href="https://b24-cfumk4.bitrix24site.ru/crm_form_wgyxv/" target="_blank">Feedback form</a></p>
+    </div>
+    
   )
 }
 
