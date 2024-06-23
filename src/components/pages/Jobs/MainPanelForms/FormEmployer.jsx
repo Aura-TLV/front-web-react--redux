834d264633494a49f10b8834671c0dbd06451e2b
Ilya Soloveychik <dj.active.17@gmail.com>
2024-06-23 04:34:29 +0300

    chore: add missing language consts + correct consts for company

--- src/components/pages/Jobs/MainPanelForms/FormEmployer.jsx
+++ src/components/pages/Jobs/MainPanelForms/FormEmployer.jsx
@@ -13,51 +13,51 @@ const FormEmployer = () => {
 
         <div className="row">
           <div className="col-md-3">
-            <label htmlFor="login">{t('mainPanel.employee.login')}:</label>
+            <label htmlFor="login">{t('mainPanel.company.login')}:</label>
             <input id="login" />
           </div>
           <div className="col-md-3">
-            <label htmlFor="eMail">{t('mainPanel.employee.eMail')}:</label>
+            <label htmlFor="eMail">{t('mainPanel.company.eMail')}:</label>
             <input id="eMail" />
           </div>
           <div className="col-md-3">
-            <label htmlFor="password">{t('mainPanel.employee.password')}:</label>
-            <p><a id="password" href="#">{t('mainPanel.employee.passwordLink')}</a></p>
+            <label htmlFor="password">{t('mainPanel.company.password')}:</label>
+            <p><a id="password" href="#">{t('mainPanel.company.passwordLink')}</a></p>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-3">
-            <label htmlFor="userName">{t('mainPanel.employee.name')}:</label>
+            <label htmlFor="userName">{t('mainPanel.company.name')}:</label>
             <input id="userName" />
           </div>
           <div className="col-md-3">
-            <label htmlFor="companyName">{t('mainPanel.employee.companyName')}:</label>
+            <label htmlFor="companyName">{t('mainPanel.company.companyName')}:</label>
             <input id="companyName" />
           </div>
           <div className="col-md-3">
-            <label htmlFor="companyType">{t('mainPanel.employee.companyType')}:</label>
+            <label htmlFor="companyType">{t('mainPanel.company.companyType')}:</label>
             <input id="companyType" />
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-12">
-            <label htmlFor="phone">{t('mainPanel.employee.phone')}:</label>
+            <label htmlFor="phone">{t('mainPanel.company.phone')}:</label>
             <input id="phone" />
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-12">
-            <p>{t('mainPanel.employee.yourAds.label')}:</p>
-            <a href="#">{t('mainPanel.employee.yourAds.btnAdd')}</a>
+            <p>{t('mainPanel.company.yourAds.label')}:</p>
+            <a href="#">{t('mainPanel.company.yourAds.btnAdd')}</a>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-12">
-            <button type="submit">{t('mainPanel.employee.controls.btnSave')}</button>
+            <button type="submit">{t('mainPanel.company.controls.btnSave')}</button>
           </div>
         </div>
 
