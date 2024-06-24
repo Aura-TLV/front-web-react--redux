f2431cedbaeb242ca63013d3752978b28f7dd472
Ilya Soloveychik <dj.active.17@gmail.com>
2024-06-24 18:05:13 +0300

    feat: add fetch for reg company form

--- src/components/pages/Registration/RegCompanyForm.jsx
+++ src/components/pages/Registration/RegCompanyForm.jsx
@@ -1,51 +1,142 @@
+import { useState } from 'react';
+import { regNewCompany, sendEmail } from '../../../utils/fetcher';
+import passwordGenerator from '../../../utils/passwordGenerator';
 import { useTranslation } from "react-i18next";
 
 const RegCompanyForm = () => {
 
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
 
-    const submitHandler = (e, finalMsg) => {
+    const getEmailTitle = (language = 'en') => {
+        if (language === 'he') return 'הסיסמה שלך - חברה';
+        if (language === 'ru') return 'ваш пароль - компания';
+        return 'your password - company';
+    };
+
+    const getEmailLoginString = (language = 'en') => {
+        if (language === 'he') return 'היכנס לאזור האישי שלך';
+        if (language === 'ru') return 'Перейти в личный кабинет';
+        return 'Log in to your personal area';
+    };
+
+    const [formData, setFormData] = useState({
+        name: '',
+        profile: '',
+        jobs: '',
+        workersQuantity: 0,
+        phone: '',
+        eMail: '',
+        workStart: '',
+        workFinish: '',
+        city: ''
+    });
+
+    const onChangeHandler = (name, value) => {
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const submitHandler = async (e, finalMsg) => {
         e.preventDefault();
+        const password = passwordGenerator();
+        const language = i18n.language.split('-')[0];
+
+        await sendEmail(getEmailTitle(language), getEmailLoginString(language), password, formData.eMail);
+
+
+        await regNewCompany({
+            name: formData.name,
+            profile: formData.profile,
+            password,
+            jobs: formData.jobs,
+            workersQuantity: formData.workersQuantity,
+            location: formData.city,
+            eMail: formData.eMail,
+            phone: formData.phone,
+            workStart: formData.workStart,
+            workEnd: formData.workFinish
+        });
+
         alert(finalMsg);
-        console.log('reg company');
     }
 
     return (
         <form onSubmit={e => submitHandler(e, t('mainPanel.reg.finalMessage'))}>
             <div className="col-md-12">
                 <label htmlFor="name">{t('mainPanel.reg.company.name')}</label>
-                <input type="text" id="name" required />
+                <input type="text" 
+                    onKeyDown={(e) => onChangeHandler('name', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('name', e.target.value)}
+                    onChange={(e) => onChangeHandler('name', e.target.value)} 
+                id="name" required />
             </div>
             <div className="col-md-12">
                 <label htmlFor="profile">{t('mainPanel.reg.company.profile')}</label>
-                <input type="text" id="profile" required />
+                <input type="text" 
+                    onKeyDown={(e) => onChangeHandler('profile', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('profile', e.target.value)}
+                    onChange={(e) => onChangeHandler('profile', e.target.value)} 
+                id="profile" required />
             </div>
             <div className="col-md-12">
                 <label htmlFor="jobs">{t('mainPanel.reg.company.positions')}</label>
-                <input type="text" id="jobs" required />
+                <input type="text" 
+                    onKeyDown={(e) => onChangeHandler('jobs', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('jobs', e.target.value)}
+                    onChange={(e) => onChangeHandler('jobs', e.target.value)} 
+                id="jobs" required />
             </div>
             <div className="col-md-12">
                 <label htmlFor="workersQuantity">{t('mainPanel.reg.company.workersQuantity')}</label>
-                <input type="number" id="workersQuantity" min={1} max={999} required />
+                <input type="number" 
+                    onMouseLeave={(e) => onChangeHandler('workersQuantity', e.target.value)}
+                    onKeyDown={(e) => onChangeHandler('workersQuantity', e.target.value)}
+                    onChange={(e) => onChangeHandler('workersQuantity', e.target.value)} 
+                id="workersQuantity" min={1} max={999} required />
+            </div>
+            <div className="col-md-12">
+                <label htmlFor="phone">{t('mainPanel.reg.company.phone')}</label>
+                <input type="text"
+                    onKeyDown={(e) => onChangeHandler('phone', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('phone', e.target.value)}
+                    onChange={(e) => onChangeHandler('phone', e.target.value)}
+                id="phone" />
             </div>
             <div className="col-md-12">
                 <label htmlFor="email">{t('mainPanel.reg.company.eMail')}</label>
-                <input type="email" id="email" required />
+                <input type="email"
+                        onKeyDown={(e) => onChangeHandler('eMail', e.target.value)}
+                        onMouseLeave={(e) => onChangeHandler('eMail', e.target.value)}
+                        onChange={(e) => onChangeHandler('eMail', e.target.value)} 
+                        id="email" 
+                required />
             </div>
             <div className="col-md-12">
                 <label htmlFor="workStart">{t('mainPanel.reg.company.workingHours.start')}</label>
-                <input type="time" id="workStart" defaultValue={'00:00'} />
+                <input type="time"
+                    onKeyDown={(e) => onChangeHandler('workStart', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('workStart', e.target.value)}
+                    onChange={(e) => onChangeHandler('workStart', e.target.value)}  
+                defaultValue={'00:00'} />
             </div>
             <div className="col-md-12">
             <label htmlFor="workFinish">{t('mainPanel.reg.company.workingHours.finish')}</label>
-            <input type="time" id="workFinish" defaultValue={'00:00'} />
+            <input type="time" 
+                    onKeyDown={(e) => onChangeHandler('workFinish', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('workFinish', e.target.value)}
+                    onChange={(e) => onChangeHandler('workFinish', e.target.value)} 
+                    id="workFinish"
+            defaultValue={'00:00'} />
             </div>
             <div className="col-md-12">
                 <label htmlFor="city">{t('mainPanel.reg.company.city')}</label>
-                <input type="text" id="city" required />
+                <input type="text"
+                    onKeyDown={(e) => onChangeHandler('city', e.target.value)}
+                    onMouseLeave={(e) => onChangeHandler('city', e.target.value)}
+                    onChange={(e) => onChangeHandler('city', e.target.value)} 
+                id="city" required />
             </div>
             <div className="col-md-12">
-            <button type="submit">{t('mainPanel.reg.btnSubmit')}</button>
+                <button type="submit">{t('mainPanel.reg.btnSubmit')}</button>
             </div>
         </form>
     )
