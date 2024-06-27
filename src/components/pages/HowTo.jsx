1f058955038415d74f0aedc5b77060b8dde7155d
Ilya Soloveychik <dj.active.17@gmail.com>
2024-06-27 13:59:46 +0300

    fix: dynamic youtube urls for each languages (mock data for now)

--- src/components/pages/HowTo.jsx
+++ src/components/pages/HowTo.jsx
@@ -4,15 +4,20 @@ import YoutubeVideo from "../common/YoutubeVideo";
 import { useTranslation } from "react-i18next";
 import { getHTML } from "../../utils/fetcher";
 
+const getYouTubeCode = (lang) => {
+  if (lang === 'en') return 'dIo9P89X6pE';
+  if (lang === 'ru') return 'Gk3tXQzCeJA';
+  return 'LXb3EKWsInQ'; // for hebrew
+}
 
 const HowTo = () => {
 
   const { t, i18n } = useTranslation();
+  const currentLang = i18n.language.split('-')[0];
 
   const [contents, setContents] = useState(null);
 
   useEffect(() => {
-    const currentLang = i18n.language.split('-')[0];
     getHTML(currentLang).then(data => {
       setContents(data)
     })
@@ -23,7 +28,7 @@ const HowTo = () => {
       <section>
         <h1>{t('nav.howTo')}</h1>
 
-        <YoutubeVideo code={'LXb3EKWsInQ'} />
+        <YoutubeVideo code={getYouTubeCode(currentLang)} />
         <hr />
         {contents && parseHTML(contents)}
 
